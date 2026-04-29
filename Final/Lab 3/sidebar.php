85179d6284d8ea9e21e86a58c09419c589e8b799
s1FaT2 <sifat.tfhfhfh321@gamil.com>
2026-04-29 18:27:01 +0600

    Add sidebar.php for navigation links in the application

---
+++ Final/Lab 3/sidebar.php
@@ -0,0 +1,19 @@
+
+<div id="sidebar">
+    <h3>Account</h3>
+
+    <ul>
+        <li><a href="dashboard.php">Dashboard</a></li>
+
+        <li><a href="profile.php">View Profile</a></li>
+        
+        <li><a href="edit_profile.php">Edit Profile</a></li>
+
+        <li><a href="change_picture.php">Change Profile Picture</a></li>
+
+        <li><a href="change_password.php">Change Password</a></li>
+        
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+
+</div>

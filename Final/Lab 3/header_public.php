5fa0ff75017ed5fa513390486a137510960b7396
s1FaT2 <sifat.tfhfhfh321@gamil.com>
2026-04-29 16:40:48 +0600

    Add header_public.php for public header layout

---
+++ Final/Lab 3/header_public.php
@@ -0,0 +1,14 @@
+<div id="header">
+    <div id="logo">
+
+    <span class="x-icon">X</span> Company
+    </div>
+
+    <div id="nav">
+        <a href="index.php">Home</a> |
+        <a href="login.php">Login</a> |
+        <a href="register.php">Registration</a>
+    </div>
+
+
+</div>
